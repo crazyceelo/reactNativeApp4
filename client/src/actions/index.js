e347e6c8f02b2516e3f1128be9188b35bb109521
Carlos Perez <solracias@gmail.com>
2018-09-20 12:00:57 -0700

    installed redux-thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 import { EMAIL_CHANGED, PASSWORD_CHANGED } from "./types";
 
 export const emailChanged = text => {
@@ -12,4 +14,11 @@ export const passwordChanged = text => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => console.log(user));
 };
