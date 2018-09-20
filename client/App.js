39a1b72ac4634511514578b3d69c2845f38a63f0
Carlos Perez <solracias@gmail.com>
2018-09-19 19:49:41 -0700

    login form complete

--- client/App.js
+++ client/App.js
@@ -5,6 +5,9 @@ import { createStore } from "redux";
 import reducers from "./src/reducers";
 import firebase from "firebase";
 
+// components
+import LoginForm from "./src/components/LoginForm";
+
 export default class App extends React.Component {
   componentWillMount() {
     // Initialize Firebase
@@ -23,9 +26,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducers)}>
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <LoginForm />
         </View>
       </Provider>
     );
