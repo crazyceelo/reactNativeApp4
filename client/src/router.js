ef6b943ffbf0116802ec19dab5c0fa8f5526aec6
Carlos Perez <solracias@gmail.com>
2018-10-01 13:10:00 -0700

    route is setup for employee list. Navigation is not complete though. Needs to connect and navigate. Initial route is login form

--- client/src/router.js
+++ client/src/router.js
@@ -5,12 +5,14 @@ import { Scene, Router } from "react-native-router-flux";
 
 // import component
 import LoginForm from "./components/LoginForm";
+import EmployeeList from "./components/EmployeeList";
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="Login" component={LoginForm} title="Please Login" />
+        <Scene key="Login" component={LoginForm} title="Please Login" initial />
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
       </Scene>
     </Router>
   );
