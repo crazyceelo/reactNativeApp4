9621de974cc5bbc76e58a4ab2f04678ddd3c42dd
Carlos Perez <solracias@gmail.com>
2018-10-05 12:10:26 -0700

    added shift section to employee list

--- client/src/components/EmployeeCreate.js
+++ client/src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Picker } from "react-native";
 import { Card, CardSection, Input, Button } from "./common";
 import { connect } from "react-redux";
 import { employeeUpdate } from "../actions";
@@ -27,6 +28,23 @@ class EmployeeCreate extends Component {
               this.props.employeeUpdate({ prop: "phone", value: text })
             }
           />
+        </CardSection>
+
+        <CardSection>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={day =>
+              this.props.employeeUpdate({ prop: "shift", value: day })
+            }
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
 
         <CardSection>
